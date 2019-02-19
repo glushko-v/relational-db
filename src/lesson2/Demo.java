b296aa978a1e9a08b1fc5b94af1c38c689f4dc1a
glushko-v <glushko@mail.com>
2019-02-19 15:26:21 +0200

    lesson4

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+
+
+        Order order = new Order(1001, "tool2212", 140, new Date(), new Date());
+    }
+}
